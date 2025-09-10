7c99405374f5b550ebbbddc5767ec7ec166265ae
jonaslekgau <jonaslekgau@gmail.com>
2025-09-10 15:41:33 +0000

    File uploader init

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import os
+from flask import Flask, request, redirect, url_for, render_template
 
-from flask import Flask, send_file
+app = Flask(__name__, template_folder='pages/templates')
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-app = Flask(__name__)
-
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return send_file('src/index.html')
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file:
+            filename = file.filename
+            os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return render_template('index.html')
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
